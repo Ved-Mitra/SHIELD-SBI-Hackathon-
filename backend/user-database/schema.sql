90a73472f813ef17756c9d0eb62b9de2e98b1f7c
Ved Mitra Verma <vedmitraverma24@gmail.com>
2026-06-25 01:34:01 +0530

    (user-database): adding new admin-aditya

--- backend/user-database/schema.sql
+++ backend/user-database/schema.sql
@@ -19,10 +19,10 @@ CREATE TABLE IF NOT EXISTS banking_users (
 INSERT INTO banking_users (username, mpin, account_number, phone_number, is_admin) VALUES
 -- Admin User
 ('admin_ved', '1234', '00000000001', '+91-9876543210', TRUE),
+('admin_aditya', '5678', '11223344556', '+91-1112223334', TRUE),
 
 -- Regular Users
-('john_doe', '5678', '11223344556', '+91-1112223334', FALSE),
-('jane_smith', '4321', '99887766554', '+91-5556667778', FALSE),
-('rohit_sharma', '1111', '33445566778', '+91-9998887776', FALSE),
-('virat_k', '9999', '55667788990', '+91-4445556667', FALSE)
+('aakarsh_sinha', '4321', '99887766554', '+91-5556667778', FALSE),
+('mayank_tiwari', '1111', '33445566778', '+91-9998887776', FALSE),
+('brijesh_thakkar', '9999', '55667788990', '+91-4445556667', FALSE)
 ON CONFLICT (username) DO NOTHING;
