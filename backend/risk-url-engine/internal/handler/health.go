8e36abb2c908299269c0f098e89761a907a1de11
Ved Mitra Verma <vedmitraverma24@gmail.com>
2026-07-02 22:36:43 +0530

    info: added health handler

---
+++ backend/risk-url-engine/internal/handler/health.go
@@ -0,0 +1,12 @@
+package handler
+
+import "net/http"
+
+func Health(w http.ResponseWriter, r *http.Request){
+	if r.Method!=http.MethodGet{
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+	w.Header().Set("Content-Type","text/plain")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("ok"))
+}
