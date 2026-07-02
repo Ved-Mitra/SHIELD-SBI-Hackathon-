ed0708a94eb10d18da3307ed464ae244f8ce63b6
Ved Mitra Verma <vedmitraverma24@gmail.com>
2026-07-02 22:32:53 +0530

    info: added the report.go file

---
+++ backend/risk-url-engine/internal/handler/report.go
@@ -0,0 +1,60 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"shield/risk-url-engine/internal/kafka"
+	"time"
+)
+
+
+
+type Report struct{
+	Url string `json:"url"`
+	DeviceId string `json:"device_id"`
+	Timestamp int64 `json:"timestamp"`
+}
+
+type Response struct{
+	Status string `json:"status"`
+	Message string `json:"message"`
+}
+
+func HandleReportPhishing(w http.ResponseWriter, r* http.Request){
+	if r.Method!=http.MethodPost {
+		http.Error(w,"Wrong Method Call",http.StatusMethodNotAllowed)
+		return
+	}
+
+	var payload Report
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err!=nil{
+		http.Error(w, `{"status":"error","message":"Wrong Json format"}`,http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	if payload.Url==""{
+		http.Error(w,"Url filed cannot be empty",http.StatusBadRequest)
+		return
+	}
+
+	if payload.DeviceId==""{
+		http.Error(w,"DeviceId cannot be empty",http.StatusBadRequest)
+		return
+	}
+
+	eventTime:=time.UnixMilli(payload.Timestamp)
+
+	go kafka.PublishPhishingEvent(kafka.PhishingEvent{DeviceId: payload.DeviceId, Url: payload.Url, Timestamp: eventTime})
+
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+
+	successResponse := Response{
+		Status: "success",
+		Message: "Phishing Url logged",
+	}
+	json.NewEncoder(w).Encode(successResponse)
+}
