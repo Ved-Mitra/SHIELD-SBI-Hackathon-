a92f023bd087a80b5932814e6ff13badbe98e9d7
Ved Mitra Verma <vedmitraverma24@gmail.com>
2026-07-02 20:24:52 +0530

    feat: added kafka producer in risk-url-engine

---
+++ backend/risk-url-engine/internal/kafka/producer.go
@@ -0,0 +1,39 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/segmentio/kafka-go"
+)
+
+var writer *kafka.Writer
+
+func InitProducer(brokerUrl string){
+	writer= &kafka.Writer{
+		Addr: kafka.TCP(brokerUrl),
+		Topic: "url-events",
+		Balancer: &kafka.LeastBytes{},
+	}
+}
+
+type AuthEvent struct{
+	UserId string `json:"user_id"`
+	Url string `json:"url"`
+}
+
+func PublishEvent(event AuthEvent) error{
+	if writer==nil{
+		fmt.Println("Kafka Writer is NULL in risk-url-engine")
+	}
+	payload, _ := json.Marshal(event)
+	return writer.WriteMessages(context.Background(),kafka.Message{Value: payload})
+}
+
+func CloseProducer() error{
+	if writer!=nil{
+		return writer.Close()
+	}
+	return nil
+}
