7b0ad3fe339aee90338189d9c2daff8dfdc7d964
Ved Mitra Verma <vedmitraverma24@gmail.com>
2026-07-04 01:34:49 +0530

    written a return statement for wrong method request in health.go in risk-url-engine

--- backend/risk-url-engine/internal/handler/health.go
+++ backend/risk-url-engine/internal/handler/health.go
@@ -5,6 +5,7 @@ import "net/http"
 func Health(w http.ResponseWriter, r *http.Request){
 	if r.Method!=http.MethodGet{
 		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
 	}
 	w.Header().Set("Content-Type","text/plain")
 	w.WriteHeader(http.StatusOK)
